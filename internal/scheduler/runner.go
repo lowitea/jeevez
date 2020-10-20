b900ea85532e124bd011d56b1b149d3b51c969f9
lowit <ea@lowit.ru>
2020-10-20 12:16:46 +0300

    master: adds zone

--- internal/scheduler/runner.go
+++ internal/scheduler/runner.go
@@ -5,13 +5,15 @@ import (
 	"github.com/jasonlvhit/gocron"
 	"github.com/lowitea/jeevez/internal/scheduler/tasks"
 	"log"
+	"time"
 )
 
 func Run(bot *tgbotapi.BotAPI) {
 	log.Printf("Scheduler has started")
 	s := gocron.NewScheduler()
+	loc, _ := time.LoadLocation("Europe/Moscow")
 
-	_ = s.Every(1).Day().At("10:00").Do(tasks.CovidTask(bot))
+	_ = s.Every(1).Day().At("12:30").Loc(loc).Do(tasks.CovidTask(bot))
 
 	<-s.Start()
 }
