dbfe19d42555413ccc869db5bb3ceb77ae6d6e96
lowit <ea@lowit.ru>
2020-10-22 01:01:14 +0300

    master: добавляет конвертацию валют

--- internal/handlers/converter.go
+++ internal/handlers/converter.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/lowitea/jeevez/internal/config"
@@ -12,8 +11,9 @@ import (
 	"strconv"
 )
 
-func getCurrencyRate() (float64, error) {
-	resp, err := http.Get("https://free.currconv.com/api/v7/convert?q=USD_RUB&compact=ultra&apiKey=d65168e35590aedbdcc5")
+func getCurrencyRate(curPair string) (float64, error) {
+	resp, err := http.Get(fmt.Sprintf("https://free.currconv.com/api/v7/convert?"+
+		"q=%s&compact=ultra&apiKey=d65168e35590aedbdcc5", curPair))
 	if err != nil {
 		log.Printf("Error get data: %s", err)
 		return 0, err
@@ -27,16 +27,43 @@ func getCurrencyRate() (float64, error) {
 		return 0, err
 	}
 
-	type apiResp struct {
-		UsdRub float64 `json:"USD_RUB"`
-	}
+	tpl := regexp.MustCompile(`{"\w{3}_\w{3}":(\d+.\d+)}`)
+	rateStr := tpl.FindStringSubmatch(string(body))[1]
 
-	var data apiResp
-	if err := json.Unmarshal(body, &data); err != nil {
+	rate, err := strconv.ParseFloat(rateStr, 64)
+	if err != nil {
 		log.Printf("Error get data: %s", err)
 		return 0, err
 	}
-	return data.UsdRub, nil
+
+	return rate, nil
+}
+
+func getCurPair(firstCur string, secCur string) string {
+	var firstElem string
+	var secElem string
+	if firstCur == "доллар" || firstCur == "долларов" {
+		firstElem = "USD"
+	}
+	if firstCur == "рубль" || firstCur == "рублей" {
+		firstElem = "RUB"
+	}
+	if firstCur == "евро" {
+		firstElem = "EUR"
+	}
+	if secCur == "рубли" {
+		secElem = "RUB"
+	}
+	if secCur == "доллары" {
+		secElem = "USD"
+	}
+	if secCur == "евро" {
+		secElem = "EUR"
+	}
+	if firstElem == secElem {
+		return ""
+	}
+	return fmt.Sprintf("%s_%s", firstElem, secElem)
 }
 
 func CurrencyConverterHandler(update tgbotapi.Update, bot *tgbotapi.BotAPI, _ *config.Config) {
@@ -46,7 +73,7 @@ func CurrencyConverterHandler(update tgbotapi.Update, bot *tgbotapi.BotAPI, _ *c
 	}
 
 	expMsg := regexp.MustCompile(
-		`(\d+)\s(доллар|долларов|рубль|рублей|евро)\sв\s(рубли|доллары|евро)`)
+		`^(\d+)\s(доллар|долларов|рубль|рублей|евро)\sв\s(рубли|доллары|евро)$`)
 
 	tokens := expMsg.FindStringSubmatch(update.Message.Text)
 
@@ -55,16 +82,18 @@ func CurrencyConverterHandler(update tgbotapi.Update, bot *tgbotapi.BotAPI, _ *c
 	}
 
 	value, _ := strconv.ParseFloat(tokens[1], 64)
-	//firstCur := tokens[2]
-	//secCur := tokens[3]
+	currencyPair := getCurPair(tokens[2], tokens[3])
 
-	curRate, err := getCurrencyRate()
-
-	if err != nil {
-		return
+	var result float64
+	if currencyPair == "" {
+		result = value
+	} else {
+		curRate, err := getCurrencyRate(currencyPair)
+		if err != nil {
+			return
+		}
+		result = value * curRate
 	}
-
-	result := value * curRate
 
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("%.2f", result))
 	msg.ReplyToMessageID = update.Message.MessageID
