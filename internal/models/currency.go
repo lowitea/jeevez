271cd34ddc403facafd01219a3c7fbe25fbc84a7
lowit <ea@lowit.ru>
2021-02-28 17:25:19 +0300

    adds_subscr: add docs for CurrencyRate

--- internal/models/currency.go
+++ internal/models/currency.go
@@ -2,6 +2,7 @@ package models
 
 import "time"
 
+// CurrencyRate модель валютной пары
 type CurrencyRate struct {
 	ID        uint      `gorm:"primaryKey"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
