d71e56b1e66e6946bbe677a67b50e273596333dd
lowit <ea@lowit.ru>
2021-04-12 01:06:29 +0300

    v3: adds test for getCurrencyRate

--- internal/handlers/currency_test.go
+++ internal/handlers/currency_test.go
@@ -2,10 +2,14 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/lowitea/jeevez/internal/models"
+	"github.com/lowitea/jeevez/internal/tools/testTools"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
+// TestGetCurPair тестирование функции определения валютной пары
 func TestGetCurPair(t *testing.T) {
 	cases := [...]struct {
 		firstCur string
@@ -30,4 +34,27 @@ func TestGetCurPair(t *testing.T) {
 			assert.Equal(t, getCurPair(c.firstCur, c.secCur), c.expPair)
 		})
 	}
+}
+
+// TestGetCurrencyRate тестирование функции получения валюты из базы
+func TestGetCurrencyRate(t *testing.T) {
+	curName := "USD_EUR"
+
+	db, _ := testTools.InitTestDB()
+	db.Where(fmt.Sprintf("name = %s", curName)).Delete(&models.CurrencyRate{})
+
+	rate, err := getCurrencyRate(db, "USD_EUR")
+
+	assert.Errorf(t, err, "record not found")
+	assert.Equal(t, 0.0, rate)
+
+	expValue := 100500.42
+	_ = db.Create(&models.CurrencyRate{
+		Value: expValue,
+		Name:  curName,
+	})
+	rate, err = getCurrencyRate(db, "USD_EUR")
+
+	require.NoError(t, err)
+	assert.Equal(t, expValue, rate)
 }
