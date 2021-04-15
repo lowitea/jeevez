f1e0d91a81e5ca63246d27bd894e7656caeb6aa1
lowit <ea@lowit.ru>
2021-04-15 19:56:26 +0300

    v3: adds tests for parseTime

--- internal/handlers/subscriptions_test.go
+++ internal/handlers/subscriptions_test.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/lowitea/jeevez/internal/models"
 	"github.com/lowitea/jeevez/internal/tools/testTools"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -45,4 +47,58 @@ func TestCmdSubscriptions(t *testing.T) {
 	cmdSubscriptions(update, botAPIMock, db)
 
 	botAPIMock.AssertExpectations(t)
+}
+
+// TestParseTimeValid проверяет функцию парсинга времени на валидных кесах
+func TestParseTimeValid(t *testing.T) {
+	cases := []struct {
+		hTime string
+		exp   int64
+	}{
+		{"11:00", 39600},
+		{"1:00", 3600},
+		{"0:07", 420},
+		{"13:59", 50340},
+		{"00:00", 0},
+		{"23:59", 86340},
+		{"00:01", 60},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("time=%s", c.hTime), func(t *testing.T) {
+			actualTime, err := parseTime(c.hTime)
+			assert.NoError(t, err)
+			assert.Equal(t, c.exp, actualTime)
+		})
+	}
+}
+
+// TestParseTimeInvalid проверяет функцию парсинга времени на невалидных кейсах
+func TestParseTimeInvalid(t *testing.T) {
+	cases := []struct {
+		hTime     string
+		expErrMsg string
+	}{
+		{"11:00:00", "tokens count error"},
+		{"00:00:00", "tokens count error"},
+		{"00:00:01", "tokens count error"},
+		{"24:00", "time interval error"},
+		{"24:05", "time interval error"},
+		{"-01:05", "time interval error"},
+		{"-1:00", "time interval error"},
+		{"-1", "tokens count error"},
+		{"1", "tokens count error"},
+		{"a", "tokens count error"},
+		{"1:v", "parse minutes error"},
+		{"d:v", "parse hours error"},
+		{"d:05", "parse hours error"},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("time=%s", c.hTime), func(t *testing.T) {
+			actualTime, err := parseTime(c.hTime)
+			assert.EqualError(t, err, c.expErrMsg)
+			assert.Equal(t, int64(0), actualTime)
+		})
+	}
 }
