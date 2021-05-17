98967a09941213c68b2e0a8c42ccd90b3a858890
lowit <ea@lowit.ru>
2021-05-17 10:44:36 +0300

    v3: adds getNowTimeInterval function and tests for it

--- internal/scheduler/subscriptions/tools.go
+++ internal/scheduler/subscriptions/tools.go
@@ -8,19 +8,25 @@ import (
 	"time"
 )
 
+// getNowTimeInterval получить округлённый временной интервал на основе текущего времени.
+// 					  Например если сейчас 17:23, вернётся 62400 и 62990,
+//					  это 17:20:00 и 17:29:50 переведённое в минуты.
+func getNowTimeInterval(now time.Time) (minTime int, maxTime int) {
+	roundedMinMinutes := now.Minute() / 10 * 10
+	minTime = now.Hour()*3600 + roundedMinMinutes*60
+	maxTime = minTime + 590
+	return
+}
+
 // Send отправляет сообщения по всем подпискам
 func Send(bot *tgbotapi.BotAPI, db *gorm.DB) {
 	log.Printf("Start send subscriptions")
 
 	loc, _ := time.LoadLocation("Europe/Moscow")
 	now := time.Now().In(loc)
-
-	roundedMinMinutes := now.Minute() / 10 * 10
-	minTime := now.Hour()*3600 + roundedMinMinutes*60
-	maxTime := minTime + 590
+	minTime, maxTime := getNowTimeInterval(now)
 
 	var chatSubscriptions []models.ChatSubscription
-
 	db.Where("time BETWEEN ? AND ?", minTime, maxTime).Find(&chatSubscriptions)
 
 	for _, chatSubscr := range chatSubscriptions {
