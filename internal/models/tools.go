9f31c2320776dbd420a9cdec95d347e98009c810
lowit <ea@lowit.ru>
2021-06-25 23:31:01 +0300

    v3: refactors MigrateAll

--- internal/models/tools.go
+++ internal/models/tools.go
@@ -8,25 +8,19 @@ import (
 // MigrateAll выполняет автомиграцию базы данных
 func MigrateAll(db *gorm.DB) error {
 	log.Print("AutoMigrating has started")
-	if err := db.AutoMigrate(&CurrencyRate{}); err != nil {
-		log.Printf("migrating CurrencyRate error: %s", err)
-		return err
+
+	err := db.AutoMigrate(
+		&CurrencyRate{},
+		&Chat{},
+		&Subscription{},
+		&ChatSubscription{},
+		&CovidStat{},
+	)
+
+	if err == nil {
+		return nil
 	}
-	if err := db.AutoMigrate(&Chat{}); err != nil {
-		log.Printf("migrating Chat error: %s", err)
-		return err
-	}
-	if err := db.AutoMigrate(&Subscription{}); err != nil {
-		log.Printf("migrating Subscription error: %s", err)
-		return err
-	}
-	if err := db.AutoMigrate(&ChatSubscription{}); err != nil {
-		log.Printf("migrating ChatSubscription error: %s", err)
-		return err
-	}
-	if err := db.AutoMigrate(&CovidStat{}); err != nil {
-		log.Printf("migrating CovidStat error: %s", err)
-		return err
-	}
-	return nil
+
+	log.Printf("AutoMigrating error: %s", err)
+	return err
 }
