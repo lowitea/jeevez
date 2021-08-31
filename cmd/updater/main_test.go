fdbb9f2a077bc4319724755369825faa53deb79c
lowit <ea@lowit.ru>
2021-08-31 11:46:58 +0300

    v3: fixes TestRunner
    v3: fixes testRunner

--- cmd/updater/main_test.go
+++ cmd/updater/main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"github.com/lowitea/jeevez/internal/config"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
@@ -24,6 +26,29 @@ func TestRunner(t *testing.T) {
 			Token string `required:"true"`
 		}{"1"},
 	}
+	if val, ok := os.LookupEnv("JEEVEZ_TEST_DB_HOST"); ok == true {
+		testCfg.DB.Host = val
+	}
+	if val, ok := os.LookupEnv("JEEVEZ_TEST_DB_USER"); ok == true {
+		testCfg.DB.User = val
+	}
+	if val, ok := os.LookupEnv("JEEVEZ_TEST_DB_PASSWORD"); ok == true {
+		testCfg.DB.Password = val
+	}
+	if val, ok := os.LookupEnv("JEEVEZ_TEST_DB_NAME"); ok == true {
+		testCfg.DB.Name = val
+	}
 
 	assert.NotPanics(t, func() { initApp(func() (*config.Config, error) { return &testCfg, nil }) })
+
+	// ошибка инициализации конфига
+	assert.PanicsWithValue(
+		t,
+		"env parse error test",
+		func() { initApp(func() (*config.Config, error) { return nil, errors.New("test") }) },
+	)
+
+	// ошибка инициализации базы
+	testCfg.DB.Host = `not_host`
+	assert.Panics(t, func() { initApp(func() (*config.Config, error) { return &testCfg, nil }) })
 }
